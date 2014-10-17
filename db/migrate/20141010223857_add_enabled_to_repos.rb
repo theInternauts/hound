ccf97fcfa22853661be8583b7a8923e491beab2a
Greg Lazarev <greg@thoughtbot.com>
2014-10-17 13:58:20 -0700

    Fix quotes

--- db/migrate/20141010223857_add_enabled_to_repos.rb
+++ db/migrate/20141010223857_add_enabled_to_repos.rb
@@ -2,7 +2,7 @@ class AddEnabledToRepos < ActiveRecord::Migration
   def up
     add_column :repos, :enabled, :boolean, default: false
     add_index :repos, :enabled
-    execute('UPDATE repos SET enabled=active')
+    execute("UPDATE repos SET enabled=active")
     change_column_null(:repos, :enabled, false)
   end
 
